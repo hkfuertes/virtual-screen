ec57aa2eb67a4d97337e8e62eb1d649254a8d5e2
Miguel Fuertes <mjfuertesf@gmail.com>
2026-01-14 23:14:37 +0100

    kind of working, basics

--- applet/applet.js
+++ applet/applet.js
@@ -1,63 +1,55 @@
 const Applet = imports.ui.applet;
 const PopupMenu = imports.ui.popupMenu;
-const Util = imports.misc.util;
-const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
-const Main = imports.ui.main;
-const Settings = imports.ui.settings;
 
-class VirtualScreenManager extends Applet.IconApplet {
-  constructor(metadata, orientation, panelHeight, instanceId) {
-    super(orientation, panelHeight, instanceId);
+class VirtualScreenApplet extends Applet.IconApplet {
+  constructor(metadata, orientation, panel_height, instance_id) {
+    super(orientation, panel_height, instance_id);
 
     this._path = metadata.path;
-    this.set_applet_icon_name("preferences-desktop-display");
-    this.set_applet_tooltip("Virtual Screen Manager");
 
-    this.settings = new Settings.AppletSettings(
-      this,
-      metadata.uuid,
-      instanceId
-    );
+    this.set_applet_icon_name('preferences-desktop-display');
+    this.set_applet_tooltip('Virtual Screen Manager');
 
+    this._items = {};
     this._buildMenu();
   }
 
+  /* Click izquierdo = abrir menú */
+  on_applet_clicked() {
+    this._refreshState();
+  }
+
+  /* ---------------- Menu ---------------- */
+
   _buildMenu() {
-    this.menu.removeAll();
+    const menu = this._applet_context_menu;
+    menu.removeAll();
 
-    let onItem = new PopupMenu.PopupMenuItem("HDMI ON");
-    onItem.connect("activate", () => {
-      this._runScript("hdmi-on.sh");
-    });
-    this.menu.addMenuItem(onItem);
+    /* HDMI ON */
+    this._items.on = new PopupMenu.PopupMenuItem('HDMI ON');
+    this._items.on.connect('activate', () => this._run('on'));
+    menu.addMenuItem(this._items.on);
 
-    let offItem = new PopupMenu.PopupMenuItem("HDMI OFF");
-    offItem.connect("activate", () => {
-      this._runScript("hdmi-off.sh");
-    });
-    this.menu.addMenuItem(offItem);
-
-    this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-    let regenItem = new PopupMenu.PopupMenuItem("Regenerar EDID");
-    regenItem.connect("activate", () => {
-      this._runRootScript("edid-generate.py");
-    });
-    this.menu.addMenuItem(regenItem);
+    /* HDMI OFF */
+    this._items.off = new PopupMenu.PopupMenuItem('HDMI OFF');
+    this._items.off.connect('activate', () => this._run('off'));
+    menu.addMenuItem(this._items.off);
   }
 
-  _runScript(scriptName) {
-    let script = `${this._path}/scripts/${scriptName}`;
-    Util.spawnCommandLine(`bash "${script}"`);
+  /* ---------------- Script runner ---------------- */
+
+  _script() {
+    return `${this._path}/bin/x11-manager.sh`;
   }
 
-  _runRootScript(scriptName) {
-    let script = `${this._path}/scripts/${scriptName}`;
-    Util.spawnCommandLine(`pkexec "${script}"`);
+  _run(cmd, args = []) {
+    const q = s => `"${String(s).replace(/["\\$`]/g, '\\$&')}"`;
+    const cmdline = [q(this._script()), q(cmd), ...args.map(q)].join(' ');
+    GLib.spawn_command_line_async(cmdline);
   }
 }
 
-function main(metadata, orientation, panelHeight, instanceId) {
-  return new VirtualScreenManager(metadata, orientation, panelHeight, instanceId);
+function main(metadata, orientation, panel_height, instance_id) {
+  return new VirtualScreenApplet(metadata, orientation, panel_height, instance_id);
 }
--- applet/bin/x11-manager.sh
+++ applet/bin/x11-manager.sh
@@ -25,35 +25,20 @@ write_sysfs() {
 }
 
 # ---------- Helpers ----------
-connector_on() {
-  [ -w "$FORCE_FILE" ] && write_sysfs on "$FORCE_FILE" || write_sysfs on "$STATUS_FILE"
+connector() {
+  [ -w "$FORCE_FILE" ] && write_sysfs on "$FORCE_FILE" || write_sysfs "$1" "$STATUS_FILE"
 }
-
-connector_off() {
-  [ -w "$FORCE_FILE" ] && write_sysfs detect "$FORCE_FILE" || write_sysfs off "$STATUS_FILE"
-}
-
-# ---------- Status ----------
-status_internal() {
-  echo "connector=$(cat "$STATUS_FILE" 2>/dev/null || echo unknown)"
-}
-
-# ---------- Args ----------
-CMD="$1"
 
 # ---------- Main ----------
-case "$CMD" in
+case "$1" in
   on)
-    connector_on
+    connector on
   ;;
   off)
-    connector_off
-  ;;
-  status)
-    status_internal
+    connector off
   ;;
   *)
-    echo "Usage: $0 on|off|status"
+    echo "Usage: $0 on|off"
     exit 1
   ;;
 esac
